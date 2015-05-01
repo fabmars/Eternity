deb45a921ecb215f5c40b0a53ea5aab6dda88f33
mars <mars@zenith>
2015-05-01 11:53:52 +0200

    adding utilities to rotate orientation

--- src/main/java/fr/esiea/glpoo/eternity/domain/Orientation.java
+++ src/main/java/fr/esiea/glpoo/eternity/domain/Orientation.java
@@ -2,17 +2,26 @@ package fr.esiea.glpoo.eternity.domain;
 
 /**
  * We'll use the ordinal as the index in the Piece.faces array.
+ * Note they are defined in a clockwise manner
  */
 public enum Orientation {
   NORTH,
-  WEST,
+  EAST,
   SOUTH,
-  EAST;
+  WEST;
   
   
-  public Orientation nextClockwise() {
+  public Orientation rotateClockwise() {
+    return rotateClockwise(1);
+  }
+  
+  public Orientation rotateClockwise(Orientation orientation) {
+    return rotateClockwise(orientation.ordinal());
+  }
+
+  public Orientation rotateClockwise(int times) {
     Orientation[] values = values();
     int size = values.length;
-    return values[(ordinal() + 1) % size];
+    return values[(ordinal() + times) % size];
   }
 }
