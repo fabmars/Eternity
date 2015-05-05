93657f30e9d5ce616b527185717ed8660ee8036b
mars <mars@zenith>
2015-05-05 02:56:10 +0200

    updated tests

--- src/test/java/fr/esiea/glpoo/eternity/PieceTest.java
+++ src/test/java/fr/esiea/glpoo/eternity/PieceTest.java
@@ -1,13 +1,17 @@
 package fr.esiea.glpoo.eternity;
 
-import java.awt.Color;
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
-import junit.framework.Assert;
+import java.awt.Color;
 
 import org.junit.Test;
 
 import fr.esiea.glpoo.eternity.domain.Face;
 import fr.esiea.glpoo.eternity.domain.FaceType;
+import fr.esiea.glpoo.eternity.domain.Orientation;
 import fr.esiea.glpoo.eternity.domain.Pattern;
 import fr.esiea.glpoo.eternity.domain.Piece;
 
@@ -18,27 +22,38 @@ public class PieceTest {
   Face f3 = new Face(4, FaceType.EDGE, Color.green, Pattern.ZIGZAG, Color.white);
   Face f4 = new Face(5, FaceType.EDGE, Color.black, Pattern.TRIANGLE, Color.cyan);
   
-  Piece p1a = new Piece(1, f1a, f2, f3, f4);
-  Piece p1b = new Piece(p1a).rotateClockwise(); //p1a rotated
-  Piece p2 = new Piece(2, f2, f1a, f3, f4);
+  Piece p1a = new Piece(1, f1a, f2, f3, f4, Orientation.SOUTH);
+  Piece p1b = new Piece(p1a).rotateClockwise(); //p1a rotated via orientation
+  Piece p1c = new Piece(2, f3, f4, f1a, f2, Orientation.WEST); //p1a rotated via array AND orientation
+  Piece p2 = new Piece(3, f2, f1a, f3, f4, Orientation.SOUTH);
   
   @Test
   public void testFaceSame() {
-    Assert.assertSame(f1a, f1b);
+    assertEquals(f1a, f1b);
   }
 
   @Test
   public void testFaceDifferent() {
-    Assert.assertNotSame(f1a, f2);
+    assertThat(f1a, not(equalTo(f2)));
   }
   
   @Test
-  public void testPieceRotatedIsSame() {
-    Assert.assertSame(p1a, p1b);
+  public void testPieceRotated1IsSame() {
+    assertEquals(p1a, p1b);
+  }
+
+  @Test
+  public void testPieceRotated2IsSame() {
+    assertEquals(p1a, p1c);
   }
   
+  @Test
+  public void testPieceRotated3IsSame() {
+    assertEquals(p1b, p1c);
+  }
+
   @Test
   public void testPieceDifferent() {
-    Assert.assertNotSame(p1a, p2);
+    assertThat(p1a, not(equalTo(p2)));
   }
 }
